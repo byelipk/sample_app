7a85d08bc4d5cc5741acb549db051600433c440d
byelipk <byelipk@gmail.com>
2013-09-19 11:51:31 -0300

    Updating users 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page] )
+  end
+
   def new
+    if signed_in?
+      redirect_to @current_user
+    end
   	@user = User.new
   end
 
@@ -8,6 +20,9 @@ class UsersController < ApplicationController
   end
 
   def create
+    if signed_in?
+      redirect_to @current_user
+    end
   	@user = User.new(params[:user])
   	if @user.save
       sign_in @user
@@ -17,4 +32,48 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit 
+    #@user = User.find(params[:id]) -- @user defined in before_filter
+  end
+
+  def update
+    #@user = User.find(params[:id]) -- @user defined in before_filter
+    if @user.update_attributes(params[:user])
+      flash.now[:success] = "Changes to your profile have been saved!"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user.admin == false
+      user.destroy
+      flash[:success] = "User account removed successfully."
+      redirect_to users_url
+    else
+      flash[:error] = "Admin account removal failed."
+      redirect_to @current_user
+    end
+  end  
+
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please log in to access this content."        
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,8 @@
 module SessionsHelper
 
+  # ARGS: User object
+  # RETURN VALUE: Stores a cookie for user authentication, 
+  # -- and instantiates @current_user
   def sign_in(user)
   	# Store user's remember token in a cookie; valid for 20 years
     cookies.permanent[:remember_token] = user.remember_token
@@ -9,20 +12,46 @@ module SessionsHelper
     self.current_user = user
   end
 
+  # ARGS: User object
+  # RETURN VALUE: @current_user
+  def current_user=(user)
+    @current_user = user
+  end 
+
+  # ARGS: none
+  # RETURN VALUE: Boolean
   def signed_in?
     !current_user.nil?
   end  
 
-  def current_user=(user)
-  	@current_user = user
-  end
-
+  # ARGS: none
+  # RETURN VALUE: 
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end  
 
+  # ARGS: User object
+  # RETURN VALUE: Boolean; compares current user to the user 
+  # -- whose page current user requested acces to 
+  def current_user?(user)
+    current_user == user
+  end
+
+  # ARGS: none
+  # RETURN VALUE: Sets @current_user to nil and deletes remember_token
   def sign_out
   	self.current_user = nil
   	cookies.delete(:remember_token)
+  end
+
+  # ARGS: 
+  # RETURN VALUE: Redirects user to requested URL, or re-routes user to default url
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
   end
 end
