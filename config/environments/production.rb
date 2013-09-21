d0e6cbe5fef9b494142395208078dcb37eaf7534
byelipk <byelipk@gmail.com>
2013-09-21 08:02:31 -0300

    Hopefully final changes to heroku email test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,10 +48,10 @@ SampleApp::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'sheltered-reaches-8554.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
