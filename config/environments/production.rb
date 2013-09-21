fd941b2ab2b3f1b672b68a1a75c110a766c720af
byelipk <byelipk@gmail.com>
2013-09-21 08:06:12 -0300

    More changes to heroku email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,12 +51,12 @@ SampleApp::Application.configure do
   config.action_mailer.default_url_options = { :host => 'sheltered-reaches-8554.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "myapp.herokuapp.com",
+    domain: "sheltered-reaches-8554.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
