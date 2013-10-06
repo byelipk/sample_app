8e121f41a745060bbcb43484f0335a93603d250d
byelipk <byelipk@gmail.com>
2013-10-06 17:34:08 -0300

    redo

--- app/mailers/registration_notification.rb
+++
@@ -1,9 +0,0 @@
-class RegistrationNotification < ActionMailer::Base
-  default from: "byelipk@gmail.com"
-
-  def welcome_email(user)
-  	@user = user
-  	@url = 'https://sheltered-reaches-8554.herokuapp.com/signin'
-  	mail(to: @user.email, subject: 'Velcom to Kshizzy!')
-  end
-end
