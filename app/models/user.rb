2ba7104b9f35dc550d32d36c803646d058baf0a5
byelipk <byelipk@gmail.com>
2013-10-09 11:58:29 -0300

    Oh yeah

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ActiveRecord::Base
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+  #validates :password_confirmation, presence: true
   after_validation { self.errors.messages.delete(:password_digest) }
 
   # -- Instance methods
